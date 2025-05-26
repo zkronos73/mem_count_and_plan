359e4b19e969f689ee28799f7f51fe06fe757181
zkronos73 <zkronos73@gmail.com>
2025-05-26 14:21:06 +0000

    wip

--- mem_types.hpp
+++ mem_types.hpp
@@ -20,5 +20,19 @@ struct MemCountTrace {
     uint32_t chunks = 0;
 };
 
+// RD_REL             |3: type(RD_REL)                 |29: relative_step   |1 x 32 bits
+// RD_ABS             |3: type(RD_ABS)                 |61: absolute_step   |2 x 32 bits
+// WR_REL_ZERO        |3: type(WR_REL_ZERO)            |29: relative_step   |1 x 32 bits
+// WR_REL_SHORT_DATA  |3: type(WR_REL_SHORT_DATA)      |29: relative_step   |2 x 32 bits
+// WR_REL_LONG_DATA   |3: type(WR_REL_LONG_DATA)       |29: relative_step   |3 x 32 bits
+// WR_ABS_SHORT_DATA  |3: type(WR_ABS_SHORT_DATA)      |29: relative_step   |3 x 32 bits
+// WR_ABS_LONG_DATA   |3: type(WR_ABS_LONG_DATA)       |29: relative_step   |4 x 32 bits
+
+struct MemTrace {
+    MemCountersBusData *chunk_data[MAX_CHUNKS];
+    uint32_t chunk_size[MAX_CHUNKS];
+    uint32_t chunks = 0;
+};
+
 
 #endif
